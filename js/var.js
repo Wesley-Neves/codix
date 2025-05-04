4aea0fa01489e9f3523495c52838e34ca1b4052e
Wesley Neves Batista <107236602+Wesley-Neves@users.noreply.github.com>
2025-05-03 21:40:43 -0400

    Add files via upload

---
+++ js/var.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: [
+      "Soluções Web",
+      "Aplicativos Mobile",
+      "Testes Automatizados",
+      "QA",
+      "Consultoria em TI",
+      "Projetos com IA",
+      "Computação em Nuvem"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
